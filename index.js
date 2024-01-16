08391fec2e4ea2734e48a3036f647785efcb7ed7
Macgyver1971 <imariful777@gmail.com>
2024-01-16 15:25:39 +0600

    adding express js and using methods

---
+++ index.js
@@ -0,0 +1,34 @@
+let express = require("express")
+//this command pull express from node packages
+let app = express()
+//this will allow to use express object features
+
+app.listen(/*port_number*/ 8080,()=>{
+  console.log("Server running on post 8080")
+})
+//app.listen will start a server on desired port and listens for request
+
+app.get(/*route*/"/",(req,res)=>{
+  res.send("Welcome to express!")
+})
+
+//app.get is a method that send respond on GET request
+//res is an object and it contains many important information EX: req.params
+
+app.get("/:username",(req,res)=>{
+  let {username} = req.params;
+  res.send(`Welcome to ${username}'s page`)
+})
+
+//  "/:username" => here we are declaring a variable
+//called username and we can access it on request 
+//like top example
+
+app.get("/home/search",(req,res)=>{
+  let {q} = req.query;
+  res.send(`You searched for ${q}`)
+})
+
+//req.query is an object that represent QUERY STRINGS
+//ex: http://localhost:8080/home/search?q=%22arif%22arif
+//here ?q="arif" after search letter is a QUERY STRING
